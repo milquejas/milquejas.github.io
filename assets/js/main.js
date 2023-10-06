94589e95cc90a3494fe9578a12c7e5d31adde6de
milquejas <84683894+milquejas@users.noreply.github.com>
2023-10-06 08:40:24 +0300

    korjatttu homepage ongelma

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const portfolioLink = document.querySelector(".page-link[href='/portfolio']");
-//     const dropdownContent = document.querySelector(".dropdown-content");
+document.addEventListener("DOMContentLoaded", function () {
+    const portfolioLink = document.querySelector(".page-link[href='/portfolio']");
+    const dropdownContent = document.querySelector(".dropdown-content");
   
-//     if (portfolioLink && dropdownContent) {
-//       portfolioLink.addEventListener("mouseenter", function () {
-//         dropdownContent.style.display = "block";
-//       });
+    if (portfolioLink && dropdownContent) {
+      portfolioLink.addEventListener("mouseenter", function () {
+        dropdownContent.style.display = "block";
+      });
   
-//       portfolioLink.addEventListener("mouseleave", function () {
-//         dropdownContent.style.display = "none";
-//       });
-//     }
-//   });
+      portfolioLink.addEventListener("mouseleave", function () {
+        dropdownContent.style.display = "none";
+      });
+    }
+  });
   
